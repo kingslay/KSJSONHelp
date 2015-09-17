1be9b29c4972a662341c93bb7227d5ad19a23bcb
wangjinbian <wangjinbian@linggan.com>
2015-09-17 18:09:31 +0800

    去除警告

--- KSJSONHelp/Helpers.swift
+++ KSJSONHelp/Helpers.swift
@@ -15,16 +15,6 @@ func findFirst<S : SequenceType> (s: S, condition: (S.Generator.Element) -> Bool
   }
   return nil
 }
-func SwiftClassFromString(className: String) -> AnyClass! {
-    // get the project name
-    if  var appName  = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleName") as! String? {
-        // generate the full name of your class (take a look into your "YourProject-swift.h" file)
-        let classStringName = "_TtC\(appName.characters.count)\(appName)\(className.characters.count)\(className)"
-        // return the class!
-        return NSClassFromString(classStringName)
-    }
-    return nil;
-}
 
 extension String{
     
